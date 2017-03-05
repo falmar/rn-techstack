0f56bc254a31864aed0c7be8409ac3f3183458b7
David Lavieri <daviddlavier@gmail.com>
2017-03-05 15:50:25 -0400

    Update ListItem

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -7,10 +7,25 @@ import * as actions from '../store/actions/libraries'
 import {CardSection} from './common'
 
 class ListItem extends Component {
+  renderDescription () {
+    const {expanded, library: {description}} = this.props
+
+    if (expanded) {
+      const {descriptionStyle} = styles
+
+      return (
+        <CardSection>
+          <Text style={descriptionStyle}>{description}</Text>
+        </CardSection>
+      )
+    }
+  }
+
   render () {
-    const {library, selected, onPress} = this.props
+    const {library, onPress} = this.props
+    const {id, title} = library
+
     const {titleStyle} = styles
-    const {id, title, description} = library
 
     return (
       <TouchableWithoutFeedback
@@ -21,7 +36,7 @@ class ListItem extends Component {
           <CardSection>
             <Text style={titleStyle}>{title}</Text>
           </CardSection>
-          {library.id === selected && <Text>{description}</Text>}
+          {this.renderDescription()}
         </View>
 
       </TouchableWithoutFeedback>
@@ -33,18 +48,25 @@ const styles = StyleSheet.create({
   titleStyle: {
     fontSize: 18,
     paddingLeft: 5
+  },
+  descriptionStyle: {
+    paddingLeft: 5,
+    paddingRight: 5,
+    flex: 1
   }
 })
 
 ListItem.propTypes = {
   library: PropTypes.object.isRequired,
-  selected: PropTypes.number,
+  expanded: PropTypes.bool,
   onPress: PropTypes.func.isRequired
 }
 
-const mapStateToProps = ({libraries}) => ({
-  selected: libraries.selected
-})
+const mapStateToProps = ({libraries}, ownProps) => {
+  return {
+    expanded: libraries.selected === ownProps.library.id
+  }
+}
 
 const mapDispatchToProps = dispatch => ({
   onPress: libraryId => () => dispatch(actions.selectLibrary(libraryId))
